32ebb6eb46f2ee886eeafc536e311ce21b6fde73
Mustafa Khan <mk9572898@gmail.com>
2023-04-27 16:48:42 +0500

    i am poor

---
+++ Iampoor/I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by MacBook Pro 2015 on 20/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
